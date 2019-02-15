6f6ce1fae772c075efd94b986461b92faeedc683
cecil john tantay <cjbtantay@gmail.com>
2019-02-15 09:00:57 -0800

    add getprojectsactions request

--- data/routers/projectsRouter.js
+++ data/routers/projectsRouter.js
@@ -6,10 +6,31 @@ router.get('/:id', (req, res) => {
   const id = req.params.id;
   db.get(id)
     .then(project => {
-      console.log(project);
       res.status(200).json(project);
     })
-    .catch();
+    .catch(() => {
+      res.status(404).json({ message: "Cannot find an ID you're looking for" });
+    });
+});
+
+router.get('/:projectId/actions', (req, res) => {
+  const projectId = req.params.projectId;
+  db.getProjectActions(projectId)
+    .then(actions => {
+      if (actions.length === 0) {
+        res
+          .status(404)
+          .json({ message: "Cannot find an ID you're looking for" });
+      } else {
+        console.log(actions);
+        res.status(200).json(actions);
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: "Something went wrong.. and it's my bad." });
+    });
 });
 
 module.exports = router;
