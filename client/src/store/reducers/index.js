c4aa99a6599ef6ad4aa976af6376dbaecff88817
cecil john tantay <cjbtantay@gmail.com>
2019-02-15 09:48:40 -0800

    add redux and CRA client for stretch

---
+++ client/src/store/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+// Change main reducer to be more specific if needed
+import mainReducer from './mainReducer';
+
+export default combineReducers({
+  mainReducer // <- temp name
+});
